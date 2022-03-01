bd0afdb1a27e0f3d3ff2ae3cff21b515f775640d
ninjaboynaru <thiagoCord755@gmail.com>
2022-03-01 13:40:10 -0500

    Repeat rendering on finish

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 const svgContainer = document.getElementsByTagName('svg')[0];
 
-stringArt.centerQuad(500,500, 100, {delay:1, width:1, color:'#BEBEBE'} ).render(svgContainer, 1);
 
+const centerQuad = stringArt.centerQuad(500,500, 100, {delay:1, width:1, color:'#BEBEBE'} );
+
+// DANGER: Possible stack overflow
+function renderCenterQuad() {
+    d3.select(svgContainer).selectAll('*').remove();
+    centerQuad.render(svgContainer, 0.5, renderCenterQuad);
+}
+
+document.addEventListener('DOMContentLoaded', renderCenterQuad);
