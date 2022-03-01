9f2b60db0a11b83513b1981916166cf71a9c2530
ninjaboynaru <thiagoCord755@gmail.com>
2022-03-01 13:23:30 -0500

    Removed unused console.log alias

--- js/index.js
+++ js/index.js
@@ -1,11 +1,4 @@
-const lx = console.log;
-
 const svgContainer = document.getElementsByTagName('svg')[0];
 
-
-//const gallery = stringArt.gallery(svgContainer, 500, 500, 10);
-
 stringArt.centerQuad(500,500, 100, {delay:1, width:1, color:'#BEBEBE'} ).render(svgContainer, 1);
-
-//lx(renderData);
 
